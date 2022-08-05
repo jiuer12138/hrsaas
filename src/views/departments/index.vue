6460cc347b5f3dfcf185017026299a3f8d234548
jiuer12138 <18815283494@163.com>
2022-08-05 11:32:56 +0800

    封装树形结构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <!-- 头部 -->
+      <el-card class="box-card">
+        <TreeTools :treeNode="company" :isRoot="true"></TreeTools>
+        <el-tree :data="treeData" :props="defaultProps" default-expand-all>
+          <template v-slot="scope">
+            <TreeTools :treeNode="scope.data"></TreeTools>
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
 export default {
   data() {
-    return {}
+    return {
+      treeData: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' }
+      ],
+      defaultProps: {
+        label: 'name'
+      },
+      company: {
+        name: '传智教育',
+        manage: '负责人'
+      }
+    }
+  },
+
+  components: {
+    TreeTools
   },
 
   created() {},
