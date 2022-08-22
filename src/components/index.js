50b174b964bacde2d738aa9d1b5143ff63b7e1b3
jiuer12138 <18815283494@163.com>
2022-08-22 14:27:41 +0800

    修改了src

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,12 @@
-import pageTools from './pageTools'
-import UploadExcel from './UploadExcel'
-import UploadImage from './UploadImage'
-const components = [pageTools, UploadExcel, UploadImage]
+import PageTools from './pageTools'
+import MyTest from '@/components/MyTest'
+import UploadExcel from '@/components/UploadExcel'
+import UploadImg from '@/components/UploadImg'
+const components = [PageTools, MyTest, UploadExcel, UploadImg]
 export default {
   install(Vue) {
     components.forEach((component) => {
       Vue.component(component.name, component)
     })
-  }
+  },
 }
